6c815d4b813c9a2641d8eb9895cb86c27e508814
mahirx <60250437+mahirx@users.noreply.github.com>
2021-03-16 21:24:32 +0600

    Create character.cpp

---
+++ C/Bangla/1_Computer_Programming_C/1_Basics_DataType_Input_Output/character.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+	char c;
+	c = 'L';
+
+
+	printf("%c\n", c);
+
+
+	scanf("%c", &c);
+	printf("%c", c);
+
+
+	// printf("_________________________________________________");
+
+
+	char ch;
+
+	ch = getchar();
+	putchar(ch);
+    
+
+    printf("....................................................\n");
+
+	return 0;
+}
