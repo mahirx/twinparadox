b73e7a871be01c1ee0d108de3e0fa60cb74afb2e
mahirx <60250437+mahirx@users.noreply.github.com>
2021-03-16 20:30:34 +0600

    Update hello.cpp

---
+++ C/Bangla/1_Computer_Programming_C/1_Basics_DataType_Input_Output/hello.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main()
+{
+    printf("hello world\n"); // this /n indictes new line
+
+    printf("mahir\n");
+    printf("labib\n");
+
+
+    printf("mahir\t");  // this /t indicates tab
+    printf("labib");
+
+    /*
+    this is 
+    a multiline comment
+    */
+    return 0;
+}
