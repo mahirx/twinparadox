b4fd389e371e19fcb260a36fd4e10e9884a7ced9
mahirx <60250437+mahirx@users.noreply.github.com>
2021-03-16 20:45:32 +0600

    Create integer.cpp

---
+++ C/Bangla/1_Computer_Programming_C/1_Basics_DataType_Input_Output/integer.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a, b;
+        
+    a = 50, b = 70; 
+
+    int c = a + b;
+    
+    printf("%d\n", c);
+
+
+    scanf(" %d %d", &a, &b);
+
+
+    c = a + b;
+
+
+    printf("%d", c);
+
+
+    return 0;
+}
